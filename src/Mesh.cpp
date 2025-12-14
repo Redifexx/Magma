a669df191b021a3710a67627c99cacfe7f045123
Gio <44237862+Redifexx@users.noreply.github.com>
2025-12-13 23:28:37 -0500

    Mesh Class

---
+++ src/Mesh.cpp
@@ -0,0 +1,47 @@
+#include "Mesh.h"
+
+using namespace Magma;
+
+Mesh::Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices)
+{
+	glGenVertexArrays(1, &m_VAO);
+	glGenBuffers(1, &m_VBO);
+	glGenBuffers(1, &m_EBO);
+
+	glBindVertexArray(m_VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
+
+	glBufferData(GL_ARRAY_BUFFER, m_Vertices.size() * sizeof(Vertex), m_Vertices.data(), GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, m_Indices.size() * sizeof(unsigned int), m_Indices.data(), GL_STATIC_DRAW);
+
+	// Vertex Positions
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE. sizeof(Vertex), (void*)0);
+
+	// Vertex Normals
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Normal));
+
+	// Vertex Texture Coords
+	glEnableVertexAttribArray(2);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, TexCoords));
+
+	// Vertex Tangents
+	glEnableVertexAttribArray(3);
+	glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Tangent));
+
+	// Vertex Bitangents
+	glEnableVertexAttribArray(4);
+	glVertexAttribPointer(4, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Bitangent));
+
+	glBindVertexArray(0);
+}
+
+void Mesh::Draw() const
+{
+	glBindVertexArray(m_VAO);
+	glDrawElements(GL_TRIANGLES, static_cast<GLsizei>(m_Indices.size()), GL_UNSIGNED_INT, 0);
+	glBindVertexArray(0);
+}
---
+++ src/include/Mesh.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <glm/glm.hpp>
+#include <vector>
+
+namespace Magma
+{
+	struct Vertex
+	{
+		glm::vec3 Position;
+		glm::vec3 Normal;
+		glm::vec2 TexCoords;
+		glm::vec3 Tangent;
+		glm::vec3 Bitangent;
+	};
+
+	class Mesh
+	{
+		public:
+			Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices) :
+				m_Vertices(vertices), m_Indices(indices);
+
+			void Draw() const;
+		private:
+			unsigned int m_VAO, m_VBO, m_EBO;
+			std::vector<Vertex> m_Vertices;
+			std::vector<unsigned int> m_Indices;
+	};
+}
